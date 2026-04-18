0310200793712b2f1cdbaa3d6ed20728068c0b9e
mateuszkaczmarczyk999 <math.k999@gmail.com>
2026-04-18 22:50:53 +0200

    adding input binding

--- Source/Rogue/RogueCharacter.cpp
+++ Source/Rogue/RogueCharacter.cpp
@@ -3,12 +3,45 @@
 
 #include "RogueCharacter.h"
 
+#include "InputActionValue.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputComponent.h"
+
 
 // Sets default values
 ARogueCharacter::ARogueCharacter()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+	SpringArmComponent->SetupAttachment(RootComponent);
+	SpringArmComponent->bUsePawnControlRotation = true;
+	
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+	CameraComponent->SetupAttachment(SpringArmComponent);
+}
+
+void ARogueCharacter::Move(const FInputActionValue& InputValue)
+{
+	const auto InputVector = InputValue.Get<FVector2D>();
+	
+	FRotator ControllerRotation = GetControlRotation();
+	ControllerRotation.Pitch = 0.0f;
+	
+	const FVector ForwardDirection = ControllerRotation.Vector();
+	const FVector RightDirection = ControllerRotation.RotateVector(FVector::RightVector);
+	
+	AddMovementInput(ForwardDirection, InputVector.X);
+	AddMovementInput(RightDirection, InputVector.Y);
+}
+
+void ARogueCharacter::Look(const FInputActionValue& InputValue)
+{
+	const auto InputVector = InputValue.Get<FVector2D>();
+	AddControllerPitchInput(InputVector.Y);
+	AddControllerYawInput(InputVector.X);
 }
 
 // Called when the game starts or when spawned
@@ -28,5 +61,9 @@ void ARogueCharacter::Tick(float DeltaTime)
 void ARogueCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent, UInputComponent>(PlayerInputComponent);
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ARogueCharacter::Move);
+	EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ARogueCharacter::Look);
 }
 
--- Source/Rogue/RogueCharacter.h
+++ Source/Rogue/RogueCharacter.h
@@ -6,6 +6,11 @@
 #include "GameFramework/Character.h"
 #include "RogueCharacter.generated.h"
 
+struct FInputActionValue;
+class UCameraComponent;
+class USpringArmComponent;
+class UInputAction;
+
 UCLASS()
 class ROGUE_API ARogueCharacter : public ACharacter
 {
@@ -16,8 +21,25 @@ public:
 	ARogueCharacter();
 
 protected:
+	
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	TObjectPtr<UInputAction> MoveAction;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	TObjectPtr<UInputAction> LookAction;
+	
+	UPROPERTY(VisibleAnywhere, Category="CameraComponent")
+	TObjectPtr<UCameraComponent> CameraComponent;
+	
+	UPROPERTY(VisibleAnywhere, Category="SpringArmComponent")
+	TObjectPtr<USpringArmComponent> SpringArmComponent;
+	
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	
+	void Move(const FInputActionValue& InputValue);
+	
+	void Look(const FInputActionValue& InputValue);
 
 public:
 	// Called every frame
