8217bb6b3c3e43d43a424c523a56aa97e90ba16e
mateuszkaczmarczyk999 <math.k999@gmail.com>
2026-04-19 13:16:38 +0200

    add simple projectile

---
+++ Source/Rogue/Projectiles/RogueMagicProjectile.cpp
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RogueMagicProjectile.h"
+
+#include "NiagaraFunctionLibrary.h"
+#include "../../../../../../../Program Files/Epic Games/UE_5.7/Engine/Plugins/FX/Niagara/Source/Niagara/Public/NiagaraComponent.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+
+ARogueMagicProjectile::ARogueMagicProjectile()
+{
+	SphereColliderComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereCollider"));
+	SphereColliderComponent->SetSphereRadius(16.0f);
+	SphereColliderComponent->SetCollisionProfileName("RogueProjectile");
+	RootComponent = SphereColliderComponent;
+	
+	PrimaryParticlesComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("PrimaryParticles"));
+	PrimaryParticlesComponent->SetupAttachment(SphereColliderComponent);
+	
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovement"));
+	ProjectileMovementComponent->InitialSpeed = 1500.0f;
+	ProjectileMovementComponent->ProjectileGravityScale = 0.0f;
+}
+
+void ARogueMagicProjectile::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	SphereColliderComponent->OnComponentHit.AddDynamic(this, &ARogueMagicProjectile::OnParticleHit);
+}
+
+void ARogueMagicProjectile::OnParticleHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	TSubclassOf<UDamageType> DamageTypeClass = UDamageType::StaticClass();
+	UGameplayStatics::ApplyDamage(OtherActor, 10.0f, GetInstigatorController(), this, DamageTypeClass);
+	
+	UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ExplosionEffectSystem, GetActorLocation());
+	
+	Destroy();
+}
---
+++ Source/Rogue/Projectiles/RogueMagicProjectile.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RogueMagicProjectile.generated.h"
+
+class USphereComponent;
+class UProjectileMovementComponent;
+class UNiagaraComponent;
+class UNiagaraSystem;
+
+UCLASS()
+class ROGUE_API ARogueMagicProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+protected:
+	
+	UPROPERTY(EditDefaultsOnly, Category="Effects")
+	TObjectPtr<UNiagaraSystem> ExplosionEffectSystem;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Effects")
+	TObjectPtr<UNiagaraComponent> PrimaryParticlesComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Components")
+	TObjectPtr<USphereComponent> SphereColliderComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Components")
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovementComponent;
+	
+	UFUNCTION()
+	void OnParticleHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	
+public:
+	ARogueMagicProjectile();
+	
+	virtual void PostInitializeComponents() override;
+};
